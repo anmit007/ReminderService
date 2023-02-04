8484ea1b9be1ceb07fdb6baeca1ece5c4f26b2ba
AnmZ <anmit.ray911@gmail.com>
2023-02-04 10:00:50 +0530

    setup a basic cron emailer

--- src/index.js
+++ src/index.js
@@ -7,19 +7,22 @@ const setupAndStartServer = async () => {
 
     // create the express object
     const app = express();
-
+    const cron = require('node-cron');
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, async () => {
        
         console.log(`Server started at ${PORT}`);
-        sendBasicEmail(
-            'support@airline.com',
-            'anmit.ray911@gmail.com',
-            'this is a testing email',
-            'good to have you onboard'
+        // sendBasicEmail(
+        //     'support@airline.com',
+        //     'anmit.ray911@gmail.com',
+        //     'this is a testing email',
+        //     'good to have you onboard'
 
-        );
+        // );
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running 2 minute ');
+          });
     });
 }
 
