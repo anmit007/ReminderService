9561d6ef97812c97093ea5a9514a173c549a9039
AnmZ <anmit.ray911@gmail.com>
2023-02-07 17:34:27 +0530

    added RabbitMQ and message Queue logic

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const {sendBasicEmail} = require('./services/email-service');
-const { PORT } = require('./config/serverConfig');
-const jobs = require('./utils/job');
-const setupAndStartServer = async () => {
 
-    const app = express();
-    const TicketController = require('./controller/ticketController');
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.post('/api/v1/tickets',TicketController.create);
-    app.listen(PORT, async () => {
-       
-        console.log(`Server started at ${PORT}`);
-        jobs();
-    });
-}
+const { PORT } = require("./config/serverConfig");
+const { createChannel } = require("./utils/messageQueue");
+const jobs = require("./utils/job");
+const setupAndStartServer = async () => {
+  const app = express();
+  const TicketController = require("./controller/ticketController");
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  const channel = await createChannel();
+
+  app.post("/api/v1/tickets", TicketController.create);
+  app.listen(PORT, async () => {
+    console.log(`Server started at ${PORT}`);
+    jobs();
+  });
+};
 
 setupAndStartServer();
+
+/* 55 */
